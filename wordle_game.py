96d9d89c010e25547249bacd6eacef2e6c09fbd7
BuildTools <unconfigured@null.spigotmc.org>
2023-11-15 19:06:17 +0900

    213123123

--- wordle_game.py
+++ wordle_game.py
@@ -9,7 +9,7 @@ turn = 0
 print("wordle game start!!!")
 while(True):
     turn += 1
-    
+    ###############
     #입력부
     correctAnswer = True
     while(correctAnswer):
@@ -46,4 +46,4 @@ while(True):
                 print(' ', inputWord[i][j], '!',sep='', end=' ')
         print()
 
-    
+
