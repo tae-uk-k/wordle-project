e057248d6a5bcd45dcb9886b24e0f3f503e09511
BuildTools <unconfigured@null.spigotmc.org>
2023-12-03 13:43:18 +0900

    something more

--- wordle_game.py
+++ wordle_game.py
@@ -2,7 +2,6 @@ import tkinter as tk
 import pandas as pd
 import random
 
-#print(df)
 '''
 window = tk.Tk()
 window.title("< Wordle Game >")
@@ -18,6 +17,12 @@ for i in range(26):
     button[i].pack(side = tk.LEFT)
 '''
 
+def checkMatch(l, str, size):
+    for i in range(size):
+        if l[i] != str[i]:
+            return False
+    return True
+
 class Wordle:
     df = pd.read_excel("./단어장전처리.xlsx", sheet_name="vocList", header = None)
     difficulty = 0
@@ -27,8 +32,9 @@ class Wordle:
     
     def __init__(self):
         df = pd.read_excel("./단어장전처리.xlsx", sheet_name="vocList", header = None)
+        #생성자, 뭘 넣어야 할지 모르겠음
     
-    def preSetting(self):
+    def getRandomKeyword(self):
         lengthList = []
         lengthSum = 0
         for v in self.difficultyArea[self.difficulty]:
@@ -45,24 +51,33 @@ class Wordle:
                 break
         self.wordLength = len(self.keyword)
         #keyword를 임의화 시켰는데 범위가 맞는지 확신하지 못하겠음.....
+        
+    def preSetting(self):
+        self.getRandomKeyword()
+
 
     def gamePlay(self):
         inputWord = [[' ' for j in range(self.wordLength)] for i in range(self.wordLength + 1)]
         wordStatus = [[0 for j in range(self.wordLength)] for i in range(self.wordLength + 1)]    # 0 - 미입력, 1 - 없음, 2 - 있지만 자리가 다름, 3 - 자리까지 같음
         turn = 0
+        gameWin = False
+        gameLose = False
         print("wordle game start!!!")
-        while(True):
+        while(not (gameWin or gameLose)):#전체 게임 루프
             turn += 1
             
             #입력부
-            correctAnswer = True
-            while(correctAnswer):
+            while(True):
+                if (turn > self.wordLength + 1):
+                    gameLose = True
+                    break
                 ans = input(">>")
-                if (len(ans) > self.wordLength):
-                    correctAnswer = True
-                else:
-                    correctAnswer = False
-                    for i in range(len(ans)):
+                if (len(ans) > self.wordLength):#실제로는 쓰지 않을 조건문
+                    continue
+                if (False):#사전에 있는지 확인해야함
+                    continue
+
+                for i in range(len(ans)):
                         inputWord[turn - 1][i] = ans[i]
                         if (ans[i] == self.keyword[i]):
                             wordStatus[turn - 1][i] = 3
@@ -75,10 +90,13 @@ class Wordle:
                                     count += 1
                             if (count == 0):
                                 wordStatus[turn - 1][i] = 1
+                break
             
             #출력부
             print("turn :", turn)
             for i in range(self.wordLength + 1):
+                if checkMatch(inputWord[i], self.keyword, self.wordLength):
+                    gameWin = True
                 for j in range(self.wordLength):
                     if (wordStatus[i][j] == 0):
                         print("___", end=' ')
@@ -89,7 +107,11 @@ class Wordle:
                     elif(wordStatus[i][j] == 3):
                         print(' ', inputWord[i][j], '!',sep='', end=' ')
                 print()
-
+        
+        if (gameWin):
+            print("이김")
+        elif (gameLose):
+            print("짐")
 
 w = Wordle()
 w.preSetting()
