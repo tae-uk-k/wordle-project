43e518e29ba619b6860d0fc7ec77d8f09adf368a
tae-uk-k <110802129+tae-uk-k@users.noreply.github.com>
2023-12-05 00:41:47 +0900

    Update wordle_game.py

--- wordle_game.py
+++ wordle_game.py
@@ -3,6 +3,7 @@ from SaveData import saveData
 
 s=saveData()
 s.start_game()
+
 w = Wordle()
 ans = input("난이도를 입력하세요(쉬움 : 0, 보통 : 1, 어려움 : 2) : ")
 w.setDifficulty(ans)
@@ -11,6 +12,16 @@ w.gamePlay()
 turn = w.getTurn()
 
 print(turn, "번째에 맞췄습니다. ")
+s.save_game_state(s.player_name, s.score)
+
+
+
+
+
+
+
+
+
 
 
 
